c52aab102f3ffa978bf4a774218048bb0ea92c33
zmyledstin <545023190@qq.com>
2018-06-05 09:45:45 +0800

    initial book information

---
+++ books_information/index.php
@@ -0,0 +1,78 @@
+<?php
+// get ID of the product to be read
+//$id = isset($_GET['id']) ? $_GET['id'] : die('ERROR: missing ID.');
+ 
+// include database and object files
+include_once 'config/database.php';
+include_once 'objects/product.php';
+include_once 'objects/category.php';
+ 
+// get database connection
+$database = new Database();
+$db = $database->getConnection();
+ 
+// prepare objects
+$product = new Product($db);
+$category = new Category($db);
+ 
+// set ID property of product to be read
+$product->id = $id;
+ 
+// read the details of product to be read
+$product->readOne();
+
+
+
+// set page headers
+$page_title = "Read One Book";
+include_once "layout_header.php";
+ 
+// read products button
+echo "<div class='right-button-margin'>";
+    echo "<a href='index.html' class='btn btn-primary pull-right'>";
+        echo "<span class='glyphicon glyphicon-list'></span> 查看全部书籍";
+    echo "</a>";
+echo "</div>";
+
+
+// HTML table for displaying a product details
+echo "<table class='table table-hover table-responsive table-bordered'>";
+ 
+    echo "<tr>";
+        echo "<td>Name</td>";
+        echo "<td>{$product->book_name}</td>";
+    echo "</tr>";
+ 
+    echo "<tr>";
+        echo "<td>Price</td>";
+        echo "<td>&#36;{$product->price}</td>";
+    echo "</tr>";
+ 
+    echo "<tr>";
+        echo "<td>Description</td>";
+        echo "<td>{$product->description}</td>";
+    echo "</tr>";
+
+
+   echo "<tr>";
+        echo "<td>Category</td>";
+        echo "<td>{$product->category}</td>";
+    echo "</tr>";
+
+ echo "<tr>";
+        echo "<td>Image</td>";
+        echo "<td>{$product->image}</td>";
+    echo "</tr>";
+  
+ 
+echo "</table>";
+
+
+
+
+
+ 
+// set footer
+include_once "layout_footer.php";
+?>
+
---
+++ books_information/objects/product.php
@@ -0,0 +1,143 @@
+<?php
+class Product{
+ 
+    // database connection and table name
+    private $conn;
+    private $table_name = "products";
+ 
+    // object properties
+    public $id;
+    public $std_id;
+    public $book_name;
+    public $category;
+    public $description;
+    public $price;
+    public $image;
+    public $timestamp;
+ 
+    public function __construct($db){
+        $this->conn = $db;
+    }
+ 
+    // create product
+    function create(){
+ 
+        //write query
+        $query = "INSERT INTO
+                    " . $this->table_name . "
+                SET
+                    std_id=:std_id, book_name=:book_name, category=:category, description=:description, price=:price,image=:imge,created=:created";
+ 
+        $stmt = $this->conn->prepare($query);
+ 
+        // posted values
+        $this->name=htmlspecialchars(strip_tags($this->name));
+        $this->price=htmlspecialchars(strip_tags($this->price));
+        $this->description=htmlspecialchars(strip_tags($this->description));
+        $this->category_id=htmlspecialchars(strip_tags($this->category_id));
+ 
+        // to get time-stamp for 'created' field
+        $this->timestamp = date('Y-m-d H:i:s');
+ 
+        // bind values 
+        $stmt->bindParam(":name", $this->name);
+        $stmt->bindParam(":price", $this->price);
+        $stmt->bindParam(":description", $this->description);
+        $stmt->bindParam(":category_id", $this->category_id);
+        $stmt->bindParam(":created", $this->timestamp);
+ 
+        if($stmt->execute()){
+            return true;
+        }else{
+            return false;
+        }
+ 
+    }
+
+
+
+    
+
+
+    function readOne(){
+ 
+    $query = "SELECT
+                book_name, category, description, price,image
+            FROM
+                " . $this->table_name . "
+            WHERE
+                id = ?
+            LIMIT
+                0,1";
+ 
+    $stmt = $this->conn->prepare( $query );
+    $stmt->bindParam(1, $this->id);
+    $stmt->execute();
+ 
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+ 
+    $this->book_name = $row['book_name'];
+    $this->category = $row['category'];
+    $this->description = $row['description'];
+    $this->price= $row['price'];
+     $this->image= $row['image'];
+    }
+
+    
+    function update(){
+ 
+    $query = "UPDATE
+                " . $this->table_name . "
+            SET
+                name = :name,
+                price = :price,
+                description = :description,
+                category_id  = :category_id
+            WHERE
+                id = :id";
+ 
+    $stmt = $this->conn->prepare($query);
+ 
+    // posted values
+    $this->name=htmlspecialchars(strip_tags($this->name));
+    $this->price=htmlspecialchars(strip_tags($this->price));
+    $this->description=htmlspecialchars(strip_tags($this->description));
+    $this->category_id=htmlspecialchars(strip_tags($this->category_id));
+    $this->id=htmlspecialchars(strip_tags($this->id));
+ 
+    // bind parameters
+    $stmt->bindParam(':name', $this->name);
+    $stmt->bindParam(':price', $this->price);
+    $stmt->bindParam(':description', $this->description);
+    $stmt->bindParam(':category_id', $this->category_id);
+    $stmt->bindParam(':id', $this->id);
+ 
+    // execute the query
+    if($stmt->execute()){
+        return true;
+    }
+ 
+    return false;
+     
+    }
+
+
+
+    // delete the product
+    function delete(){
+ 
+    $query = "DELETE FROM " . $this->table_name . " WHERE id = ?";
+     
+    $stmt = $this->conn->prepare($query);
+    $stmt->bindParam(1, $this->id);
+ 
+    if($result = $stmt->execute()){
+        return true;
+    }else{
+        return false;
+    }
+    }
+
+
+}
+?>
